7903ac4468906762676f6d29a05ce6ae7fcbfeed
Jaipal Dhillon <jaipaldhillon@gmail.com>
2023-08-23 19:19:37 -0400

    Refactor - simplify time calculations

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,8 +1,8 @@
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import ms from "ms";
 import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
-import { all } from "axios";
 
 const apiClient = new APIClient<Game>("/games");
 
@@ -33,7 +33,7 @@ useInfiniteQuery<FetchResponse<Game>, Error>({
         getNextPageParam: (lastPage, allPages) => {
             return lastPage.next ? allPages.length + 1 : undefined;
         },
-        staleTime: 24 * 60 * 60 * 1000, // 24 hours
+        staleTime: ms('24h'),
     });
 
 export default useGames
