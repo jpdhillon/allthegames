a1de9ae4a9f3e7f7d3f4e8217f428adae97bc309
Jaipal Dhillon <jaipaldhillon@gmail.com>
2023-08-24 18:21:56 -0400

    Handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,11 +3,13 @@ import Layout from "./pages/Layout";
 import { Children } from "react";
 import HomePage from "./pages/HomePage";
 import GameDetailPage from "./pages/GameDetailPage";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: "game/:id", element: <GameDetailPage /> },
